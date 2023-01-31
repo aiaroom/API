b1f971b09366aafa28b31576478c21c02ac516f3
alice <68787830+aiaroom@users.noreply.github.com>
2023-02-01 01:27:07 +0500

    Update index.php

--- API/v1/index.php
+++ API/v1/index.php
@@ -1,10 +1,9 @@
     <?php
-// Я сделал корректный вывод по автору :)
     include_once('config.php');
     include_once('err_handler.php');
     include_once('db_connect.php');
     include_once('functions.php');
-    //include_once('find_token.php');
+
 
 ///добавить автора
     if(preg_match_all("/^(add_creator)$/ui", $_GET['type'])){
