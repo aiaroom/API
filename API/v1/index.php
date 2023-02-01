6e4dbe8ec54b6a5b1d912a34bb48acaa2300c861
alice <68787830+aiaroom@users.noreply.github.com>
2023-02-02 00:35:26 +0500

    Update index.php

--- API/v1/index.php
+++ API/v1/index.php
@@ -36,14 +36,14 @@
         if(isset($_GET['first_name']) && iconv_strlen($_GET['first_name']) > 0){
             $query .= "`first_name`,";
         }
-        if(iconv_strlen($second_name) == 0 || preg_match_all("/^(NULL)$/ui", $second_name)){
+       
             $query .= " `second_name`, ";
-        } 
+        
 
 
-        if(iconv_strlen($middle_name) == 0 || preg_match_all("/^(NULL)$/ui", $middle_name)){
+        
             $query .= " `middle_name`";
-        }
+        
 
         
         $query .= ") VALUES (";
@@ -225,7 +225,7 @@
 
         
 
-        $query = "INSERT INTO `art`(`name`,`year_create`,`creater`,`hall`) VALUES ('".$_GET['name']."','".$_GET['year_create']."',
+        $query = "INSERT INTO `art`(`name`,`year_create`,`creater_id`,`hall_id`) VALUES ('".$_GET['name']."','".$_GET['year_create']."',
         '".$_GET['creater']."','".$_GET['hall']."')";
 
         $res_query = mysqli_query($connection, $query);
@@ -338,7 +338,7 @@
         exit();
     } 
 
-// обнавить местоположение картины
+//обнавить местоположение картины 2
 else if(preg_match_all("/^(update_location_art)$/ui", $_GET['type'])){
     if(!isset($_GET['hall'])){
         echo ajax_echo(
@@ -350,18 +350,67 @@ else if(preg_match_all("/^(update_location_art)$/ui", $_GET['type'])){
         );
         exit();
     }
-    if(!isset($_GET['name'])){
-        echo ajax_echo(
-            "Ошибка!",
-            "Вы не указали Get параметр name!",
-            true,
-            "ERROR",
-            null
-        );
-        exit();
-    }
 
-    $query = "UPDATE `art` SET `hall_id`= '".$_GET['hall']."' WHERE `name` = '".$_GET['name']."'";
+        $name = '';
+        if(isset($_GET['name'])){
+          $name = $_GET['name'];
+        }
+
+
+        $first_name = '';
+        if(isset($_GET['first_name'])){
+          $first_name = $_GET['first_name'];
+        }
+        $second_name = '';
+        if(isset($_GET['second_name'])){
+          $second_name = $_GET['second_name'];
+        }
+        $middle_name = '';
+        if(isset($_GET['middle_name'])){
+          $middle_name = $_GET['middle_name'];
+        }
+
+        $query = "UPDATE `art` SET `hall_id`= '" . $_GET['hall'] . "'";
+        
+        $query .= " INNER JOIN `creaters` on `art`.`creater_id` = `creaters`.`id`";
+
+        $query .= " WHERE ";
+        if (iconv_strlen($name) > 0 ) {
+            $query .= " `hall_id`= '".$name."'";
+        } 
+        if (iconv_strlen($name) > 0 && (iconv_strlen($first_name) > 0 || iconv_strlen($second_name) > 0 ||(iconv_strlen($middle_name) > 0 ) )) {
+            $query .= " AND ";
+        }
+
+
+        if (isset($first_name) && iconv_strlen($first_name) > 0) {
+            $query .= "`creaters`.`first_name`= '" . $_GET['first_name']. "'";
+        } else {
+            $query .= "";
+        }
+
+
+
+        if (iconv_strlen($second_name) == 0 || preg_match_all("/^(NULL)$/ui", $second_name)) {
+            $query .= " ";
+        } 
+        else {
+            $query .= " AND `creaters`.`second_name`='" .$second_name. "'";
+        }
+
+
+        if (iconv_strlen($middle_name) == 0 || preg_match_all("/^(NULL)$/ui", $middle_name)) {
+            $query .= " ";
+        } 
+        else {
+            $query .= " AND `creaters`.`middle_name`='" .$middle_name. "'";
+        }
+
+        $query .= ";";
+
+
+
+        // WHERE `name` = '".$_GET['name']."'";
 
     $res_query = mysqli_query($connection, $query);
 
@@ -389,6 +438,61 @@ else if(preg_match_all("/^(update_location_art)$/ui", $_GET['type'])){
     );
     exit();
 } 
+
+
+
+
+// // обнавить местоположение картины
+// else if(preg_match_all("/^(update_location_art)$/ui", $_GET['type'])){
+//     if(!isset($_GET['hall'])){
+//         echo ajax_echo(
+//             "Ошибка!",
+//             "Вы не указали Get параметр hall!",
+//             true,
+//             "ERROR",
+//             null
+//         );
+//         exit();
+//     }
+//     if(!isset($_GET['name'])){
+//         echo ajax_echo(
+//             "Ошибка!",
+//             "Вы не указали Get параметр name!",
+//             true,
+//             "ERROR",
+//             null
+//         );
+//         exit();
+//     }
+
+//     $query = "UPDATE `art` SET `hall_id`= '".$_GET['hall']."' WHERE `name` = '".$_GET['name']."'";
+
+//     $res_query = mysqli_query($connection, $query);
+
+//     if(!$res_query){
+//         echo ajax_echo(
+//             "Ошибка!",
+//             "Ошибка в запросе!",
+//             true,
+//             "ERROR",
+//             null
+//         );
+//         exit();
+//     }
+    
+//     $ip = get_ip();
+//     $query = "INSERT INTO ip_logs (`ip`,`action`) VALUES ('".$ip."','".$_GET['type']."')";
+//     $res=mysqli_query($connection, $query);
+
+//     echo ajax_echo(
+//         "Уcпех!",
+//         "Местоположение картины изменено в бд!",
+//         false,
+//         "SUCCESS",
+//         null
+//     );
+//     exit();
+// } 
 
 
 // список картин в зале
@@ -441,7 +545,7 @@ if(preg_match_all("/^(list_art|lst_art)$/ui", $_GET['type'])){
             if (iconv_strlen($second_name) == 0 || preg_match_all("/^(NULL)$/ui", $second_name)) {
                 $query .= " ";
             } else {
-                $query .= "AND `creaters`.`second_name`='" . $second_name . "',";
+                $query .= "AND `creaters`.`second_name`='" . $second_name . "'";
             }
 
             if (iconv_strlen($middle_name) == 0 || preg_match_all("/^(NULL)$/ui", $middle_name)) {
